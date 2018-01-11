00557a1d9a7f845d885c38e696f6d679b3b6c2fd
Michał Parzyszek <michal.parzyszek@Creadhoc.local>
2018-01-11 18:52:16 +0100

    Demo link change

--- docs/index.js
+++ docs/index.js
@@ -5,7 +5,7 @@ window.onload = function () {
     var ClientPortal = PerfectGym.ClientPortal;
 
     var options = {
-        url: "http://pure.waw-parzyszek7.creadhoc.local/ClientPortal2",
+        url: "https://presentation.perfectgym.pl/ClientPortal2/#/Login",
         onConnect() {
             console.info('Connected to Client Portal');
 
