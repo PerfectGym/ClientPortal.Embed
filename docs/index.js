80744165ec82e5249fe8fdbd54cb98f4fbdecf82
l.kowalski <l.kowalski@perfectgym.com>
2018-01-17 15:05:23 +0100

    Added mobile menu.

--- docs/index.js
+++ docs/index.js
@@ -5,7 +5,7 @@ window.onload = function () {
     var ClientPortal = PerfectGym.ClientPortal;
 
     var options = {
-        url: "https://presentation.perfectgym.pl/ClientPortal2/",
+        url: "http://pure.waw-lukaszko.creadhoc.local/ClientPortal2/",
         onConnect() {
             console.info('Connected to Client Portal');
 
@@ -26,14 +26,14 @@ window.onload = function () {
     var element = document.getElementById('pg-client-portal');
 
     if (element instanceof HTMLElement) {
-      var CP = new ClientPortal(element, options);
+        var CP = new ClientPortal(element, options);
     }
 
     window.getUserData = () => {
         CP.isUserLoggedIn()
             .then((data) => {
                 var navigationElement = document.getElementById('page-header');
-                
+
                 if (data.isAuthenticated) {
                     navigationElement.classList.add('.user-logged-in');
                 } else {
@@ -47,7 +47,7 @@ window.onload = function () {
         CP.logout()
             .then(() => {
                 var navigationElement = document.getElementById('page-header');
-                
+
                 navigationElement.classList.remove('.user-logged-in');
             })
     }
@@ -55,5 +55,23 @@ window.onload = function () {
     window.goTo = (stateName, params) => {
         CP.goTo(ClientPortal.State[stateName], params)
     }
-}
 
+    var burger = document.querySelector('.burger img');
+    var mobileList = document.querySelector('.mobile-list');
+
+    burger.addEventListener('click', function () {
+        if (mobileList.classList.contains('hidden')) {
+            mobileList.classList.remove('hidden');
+        } else {
+            mobileList.classList.add('hidden');
+        }
+    })
+
+    var links = document.querySelectorAll('.mobile-list .nav-link');
+
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', function () {
+            mobileList.classList.add('hidden');
+        })
+    }
+}
